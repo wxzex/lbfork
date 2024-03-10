4c513ed8f5a55bed4a49c19d4b65047b89c901d8
falixfresh <kamilovdanya1337@gmail.com>
2024-03-10 23:15:14 +0800

    Update Notifications.kt

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Notifications.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Notifications.kt
@@ -1,166 +1,270 @@
+/**
+ * SpaceKing OpenSource Free Share
+ */
 package net.ccbluex.liquidbounce.ui.client.hud.element.elements
+
 import net.ccbluex.liquidbounce.LiquidBounce
-import net.ccbluex.liquidbounce.utils.timer.MSTimer
-import net.ccbluex.liquidbounce.LiquidBounce.hud
+import net.ccbluex.liquidbounce.features.module.modules.render.HUD
 import net.ccbluex.liquidbounce.ui.client.hud.designer.GuiHudDesigner
 import net.ccbluex.liquidbounce.ui.client.hud.element.Border
 import net.ccbluex.liquidbounce.ui.client.hud.element.Element
 import net.ccbluex.liquidbounce.ui.client.hud.element.ElementInfo
 import net.ccbluex.liquidbounce.ui.client.hud.element.Side
-import net.ccbluex.liquidbounce.utils.render.AnimationUtils
-
-import net.minecraft.client.renderer.GlStateManager
-
 import net.ccbluex.liquidbounce.ui.font.Fonts
-import net.ccbluex.liquidbounce.utils.ClientUtils
+import net.ccbluex.liquidbounce.utils.render.EaseUtils
+import net.ccbluex.liquidbounce.utils.render.GLUtils
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
-import net.minecraft.util.ResourceLocation
+import net.minecraft.client.renderer.GlStateManager
+import org.lwjgl.opengl.GL11
 import java.awt.Color
-@ElementInfo(name = "Notifications", single = true)
-class Notifications(x: Double = 0.0, y: Double = 30.0, scale: Float = 1F,
-                    side: Side = Side(Side.Horizontal.RIGHT, Side.Vertical.DOWN)) : Element(x, y, scale, side) {
+import java.math.BigDecimal
+import kotlin.math.pow
 
+/**
+ * CustomHUD Notification element
+ */
+@ElementInfo(name = "Notifications", single = true)
+class Notifications(x: Double = 0.0, y: Double = 0.0, scale: Float = 1F,
+                    side: Side = Side(Side.Horizontal.RIGHT, Side.Vertical.DOWN)) : Element(x, y, scale, side) {
     /**
      * Example notification for CustomHUD designer
      */
-    private val exampleNotification = Notification("Example Notification", Notification.Type.INFO)
+    private val exampleNotification = Notification("Notification", "This is an example notification.", NotifyType.INFO)
 
     /**
      * Draw element
      */
-    override fun drawElement(): Border? {
-        var animationY = 30F
+    override fun drawElement(partialTicks: Float): Border? {
         val notifications = mutableListOf<Notification>()
-        for(i in hud.notifications)
-            notifications.add(i)
-        for(i in notifications)
-            if(mc.currentScreen !is GuiHudDesigner)
-            i.drawNotification(animationY).also { animationY += 32 }
-        else
-            exampleNotification.drawNotification(animationY)
+        //FUCK YOU java.util.ConcurrentModificationException
+        for ((index, notify) in LiquidBounce.hud.notifications.withIndex()) {
+            GL11.glPushMatrix()
+
+            if (notify.drawNotification(index)) {
+                notifications.add(notify)
+            }
+
+            GL11.glPopMatrix()
+        }
+        for (notify in notifications) {
+            LiquidBounce.hud.notifications.remove(notify)
+        }
+
         if (mc.currentScreen is GuiHudDesigner) {
-            if (!hud.notifications.contains(exampleNotification))
-                hud.addNotification(exampleNotification)
+            if (!LiquidBounce.hud.notifications.contains(exampleNotification))
+                LiquidBounce.hud.addNotification(exampleNotification)
 
-            exampleNotification.fadeState = Notification.FadeState.STAY
-            exampleNotification.x = exampleNotification.textLength + 8F
+            exampleNotification.fadeState = FadeState.STAY
+            exampleNotification.displayTime = System.currentTimeMillis()
+//            exampleNotification.x = exampleNotification.textLength + 8F
 
-            return Border(-98F, -58F, 0F, -30F)
+            return Border(-exampleNotification.width.toFloat() + 80, -exampleNotification.height.toFloat()-24.5f, 80F, -24.5F)
         }
 
         return null
     }
 
 }
-class Notification(message : String,type : Type) {
-    var x = 0f
-    var textLength = 0
-    private var stay = 0f
-    private var fadeStep = 0f
+
+class Notification(val title: String, val content: String, val type: NotifyType, val time: Int = 2000, val animeTime: Int = 500) {
+    val height = 30
     var fadeState = FadeState.IN
-    private var stayTimer = MSTimer()
-    private var firstY = 0f
-    private var animeTime: Long = 0
-    private var message: String = ""
-    private var type: Type
-    init {
-        this.message = message
-        this.type = type
-        this.firstY = 1919F
-        this.stayTimer.reset()
-        this.textLength = Fonts.font35.getStringWidth(message)
-    }
-    enum class Type {
-        SUCCESS,
-        INFO,
-        WARNING,
-        ERROR
-    }
+    var nowY = -height
+    var string = ""
+    var displayTime = System.currentTimeMillis()
+    var animeXTime = System.currentTimeMillis()
+    var animeYTime = System.currentTimeMillis()
+    val width = Fonts.font32.getStringWidth(content) + 53
 
-    enum class FadeState {
-        IN,STAY,OUT,END
-    }
-
-    fun drawNotification(animationY: Float) {
-        val delta = RenderUtils.deltaTime
-        val width = textLength.toFloat() + 8.0f
-        var y = animationY
-        if (firstY == 1919.0F) {
-            firstY = y
+    fun drawCircle(x: Float, y: Float, radius: Float, start: Int, end: Int) {
+        GlStateManager.enableBlend()
+        GlStateManager.disableTexture2D()
+        GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO)
+        GL11.glEnable(GL11.GL_LINE_SMOOTH)
+        GL11.glLineWidth(2f)
+        GL11.glBegin(GL11.GL_LINE_STRIP)
+        var i = end.toFloat()
+        while (i >= start) {
+            var c = RenderUtils.getGradientOffset(Color(HUD.r.get(),HUD.g.get(),HUD.b.get()), Color(HUD.r2.get(),HUD.g2.get(),HUD.b2.get(), 1), (Math.abs(System.currentTimeMillis() / 360.0 + (i* 34 / 360) * 56 / 100) / 10)).rgb
+            val f2 = (c shr 24 and 255).toFloat() / 255.0f
+            val f22 = (c shr 16 and 255).toFloat() / 255.0f
+            val f3 = (c shr 8 and 255).toFloat() / 255.0f
+            val f4 = (c and 255).toFloat() / 255.0f
+            GlStateManager.color(f22, f3, f4, f2)
+            GL11.glVertex2f(
+                (x + Math.cos(i * Math.PI / 180) * (radius * 1.001f)).toFloat(),
+                (y + Math.sin(i * Math.PI / 180) * (radius * 1.001f)).toFloat()
+            )
+            i -= 360f / 90.0f
         }
-        if (firstY > y) {
-            val cacheY = firstY - (firstY - y) * ((System.currentTimeMillis() - animeTime).toFloat() / 300.0f)
-            if (cacheY <= y) {
-                firstY = cacheY
+        GL11.glEnd()
+        GL11.glDisable(GL11.GL_LINE_SMOOTH)
+        GlStateManager.enableTexture2D()
+        GlStateManager.disableBlend()
+    }
+    /**
+     * Draw notification
+     */
+    /**
+     * SpaceKing OpenSource Free Share
+     */
+    fun drawNotification(index: Int): Boolean {
+        val realY = -(index + 1) * (height + 10)
+        val nowTime = System.currentTimeMillis()
+        //Y-Axis Animation
+        if (nowY != realY) {
+            var pct = (nowTime - animeYTime) / animeTime.toDouble()
+            if (pct > 1) {
+                nowY = realY
+                pct = 1.0
+            } else {
+                pct = easeOutBack(pct)
             }
-            y = cacheY
+            GL11.glTranslated(0.0, (realY - nowY) * pct, 0.0)
         } else {
-            firstY = y
-            animeTime = System.currentTimeMillis()
+            animeYTime = nowTime
         }
-        RenderUtils.drawRect(-x + 8 + textLength, -y, -x - 5, -28F - y, Color(26,26,26).rgb)
-        RenderUtils.drawRect(-x -1, -y, -x - 5, -28F - y, when(type) {
-            Type.SUCCESS -> Color(80, 255, 80).rgb
-            Type.ERROR -> Color(255, 80, 80).rgb
-            Type.INFO -> Color(80, 80, 255).rgb
-            Type.WARNING -> Color(255, 255, 80).rgb
-        })
-        var replacedMessage = message
-        replacedMessage = replacedMessage.replace("Enabled ", "")
-        replacedMessage = replacedMessage.replace("Disabled ", "")
-        if(message.contains("Enabled", true) || message.contains("Disabled", true)) {
-            val stringBuilder = StringBuilder()
-            stringBuilder.append("$replacedMessage Module")
-            replacedMessage = stringBuilder.toString()
-        }
-        Fonts.font35.drawString(replacedMessage, -x + 2, -11F - y, Color(110, 110, 110).rgb)
-        Fonts.font40.drawString(if(message.contains("Enabled")) "Enabled" else if(message.contains("Disabled")) "Disabled" else type.toString(), -x + 2, -23F - y,
-            if(!message.contains("Enabled") && !message.contains("Disabled"))
-                when(type) {
-                Type.SUCCESS -> Color(80, 255, 80).rgb
-                Type.ERROR -> Color(255, 80, 80).rgb
-                Type.INFO -> Color(80, 80, 255).rgb
-                Type.WARNING -> Color(255, 255, 0).rgb
-                }
-            else
-                if(message.contains("Enabled"))
-                    Color(80, 255, 80).rgb
-                else
-                    Color(255, 80, 80).rgb
-        )
-        GlStateManager.resetColor()
+        GL11.glTranslated(0.0, nowY.toDouble(), 0.0)
+
+        //X-Axis Animation
+        var pct = (nowTime - animeXTime) / animeTime.toDouble()
         when (fadeState) {
             FadeState.IN -> {
-                if (x < width) {
-                    x = AnimationUtils.easeOut(fadeStep, width) * width
-                    fadeStep += delta / 4F
-                }
-                if (x >= width) {
+                if (pct > 1) {
                     fadeState = FadeState.STAY
-                    x = width
-                    fadeStep = width
+                    animeXTime = nowTime
+                    pct = 1.0
                 }
-
-                stay = 60F
+                pct = easeOutBack(pct)
             }
 
             FadeState.STAY -> {
-                if (stay > 0) {
-                    stay = 0F
-                    stayTimer.reset()
-                }
-                if (stayTimer.hasTimePassed(1500L))
+                pct = 1.0
+                if ((nowTime - animeXTime) > time) {
                     fadeState = FadeState.OUT
+                    animeXTime = nowTime
+                }
             }
 
-            FadeState.OUT -> if (x > 0) {
-                x = AnimationUtils.easeOut(fadeStep, width) * width
-                fadeStep -= delta / 4F
-            } else
-                fadeState = FadeState.END
+            FadeState.OUT -> {
+                if (pct > 1) {
+                    fadeState = FadeState.END
+                    animeXTime = nowTime
+                    pct = 1.0
+                }
+                pct = 1 - easeInBack(pct)
+            }
 
-            FadeState.END -> hud.removeNotification(this)
+            FadeState.END -> {
+                return true
+            }
         }
+        if (type.toString() == "SUCCESS") {
+            string = "a"
+        }
+        if (type.toString() == "ERROR") {
+            string = "B"
+        }
+        if (type.toString() == "WARNING") {
+            string = "D"
+        }
+        if (type.toString() == "INFO") {
+            string = "C"
+        }
+        GL11.glScaled(pct,pct,pct)
+        GL11.glTranslatef(-width.toFloat()/2 , -height.toFloat()/2, 0F)
+        RenderUtils.drawRect(0F, 0F, width.toFloat(), height.toFloat(), Color(63, 63, 63, 140))
+        drawGradientSideways(0.0, height - 1.7,
+            (width * ((nowTime - displayTime) / (animeTime * 2F + time))).toDouble(), height.toDouble(), Color(HUD.r.get(),HUD.g.get(),HUD.b.get()).rgb, Color(HUD.r2.get(),HUD.g2.get(),HUD.b2.get()).rgb)
+        Fonts.font37.drawStringWithShadow("$title", 24.5F, 7F, Color.WHITE.rgb)
+        Fonts.font32.drawStringWithShadow("$content" + " (" + BigDecimal(((time - time * ((nowTime - displayTime) / (animeTime * 2F + time))) / 1000).toDouble()).setScale(1, BigDecimal.ROUND_HALF_UP).toString() + "s)", 24.5F, 17.3F, Color.WHITE.rgb)
+        drawFilledCircle(13, 15, 8.5F,Color.BLACK)
+        Fonts.Nicon80.drawString(string, 3, 8, Color.WHITE.rgb)
+        drawCircle(12.9f,15.0f,8.8f, 0,360)
+        GlStateManager.resetColor()
+
+
+        return false
+    }
+
+    fun drawGradientSideways(left: Double, top: Double, right: Double, bottom: Double, col1: Int, col2: Int) {
+        val f = (col1 shr 24 and 0xFF) / 255.0f
+        val f1 = (col1 shr 16 and 0xFF) / 255.0f
+        val f2 = (col1 shr 8 and 0xFF) / 255.0f
+        val f3 = (col1 and 0xFF) / 255.0f
+        val f4 = (col2 shr 24 and 0xFF) / 255.0f
+        val f5 = (col2 shr 16 and 0xFF) / 255.0f
+        val f6 = (col2 shr 8 and 0xFF) / 255.0f
+        val f7 = (col2 and 0xFF) / 255.0f
+        GL11.glEnable(3042)
+        GL11.glDisable(3553)
+        GL11.glBlendFunc(770, 771)
+        GL11.glEnable(2848)
+        GL11.glShadeModel(7425)
+        GL11.glPushMatrix()
+        GL11.glBegin(7)
+        GL11.glColor4f(f1, f2, f3, f)
+        GL11.glVertex2d(left, top)
+        GL11.glVertex2d(left, bottom)
+        GL11.glColor4f(f5, f6, f7, f4)
+        GL11.glVertex2d(right, bottom)
+        GL11.glVertex2d(right, top)
+        GL11.glEnd()
+        GL11.glPopMatrix()
+        GL11.glEnable(3553)
+        GL11.glDisable(3042)
+        GL11.glDisable(2848)
+        GL11.glShadeModel(7424)
+    }
+
+    fun drawFilledCircle(xx: Int, yy: Int, radius: Float, color: Color) {
+        val sections = 50
+        val dAngle = 2 * Math.PI / sections
+        var x: Float
+        var y: Float
+        GL11.glPushAttrib(GL11.GL_ENABLE_BIT)
+        GLUtils.glEnable(GL11.GL_BLEND)
+        GLUtils.glDisable(GL11.GL_TEXTURE_2D)
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
+        GLUtils.glEnable(GL11.GL_LINE_SMOOTH)
+        GL11.glBegin(GL11.GL_TRIANGLE_FAN)
+        for (i in 0 until sections) {
+            x = (radius * Math.sin(i * dAngle)).toFloat()
+            y = (radius * Math.cos(i * dAngle)).toFloat()
+            GL11.glColor4f(color.red / 255f, color.green / 255f, color.blue / 255f, color.alpha / 255f)
+            GL11.glVertex2f(xx + x, yy + y)
+        }
+        GlStateManager.color(0f, 0f, 0f)
+        GL11.glEnd()
+        GL11.glPopAttrib()
+    }
+
+    fun easeInBack(x: Double): Double {
+        val c1 = 1.70158
+        val c3 = c1 + 1
+
+        return c3 * x * x * x - c1 * x * x
+    }
+
+    fun easeOutBack(x: Double): Double {
+        val c1 = 1.70158
+        val c3 = c1 + 1
+
+        return 1 + c3 * (x - 1).pow(3) + c1 * (x - 1).pow(2)
     }
 }
+
+enum class NotifyType() {
+    SUCCESS(),
+    ERROR(),
+    WARNING(),
+    INFO();
+}
+
+
+enum class FadeState { IN, STAY, OUT, END }
+/**
+ * SpaceKing OpenSource Free Share
+ */
+
 
